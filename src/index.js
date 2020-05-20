caba17aac03d6887b792120bcfed29ffd62fdde0
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2020-05-21 01:34:20 +0600

    feat: add scss-loader dependence

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import './styles/style.css';
+import './styles/css/style.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+import './styles/scss/style.scss';
 
 console.log('Hello');
