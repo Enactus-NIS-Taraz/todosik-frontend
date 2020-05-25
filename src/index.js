07770da347be1760551afe8b2dfde2a1766c77d5
Sultan <sutan.fb29@gmail.com>
2020-05-26 01:05:44 +0600

    feat: create cards input layout

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import './styles/scss/style.scss';
 import './styles/scss/button.scss';
+import './styles/scss/inputs.scss';
 import './js/columnAdd';
 
 
