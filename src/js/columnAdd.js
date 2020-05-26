ac909ebace9dd7f4481bd8a8a7f9cd93b7503134
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2020-05-26 14:57:02 +0600

    feat: rewrite column node in js, add function of removing column

--- src/js/columnAdd.js
+++ src/js/columnAdd.js
@@ -1,10 +1,64 @@
 const button = document.getElementById('button');
-const column = document.getElementById('column');
+
 const container = document.getElementById('container');
 
+// function addColumn() {
+//   const clone = column.cloneNode(true);
+//   container.insertBefore(clone, button);
+// }
+
 function addColumn() {
-  const clone = column.cloneNode(true);
-  container.insertBefore(clone, button);
+  const newColumn = document.createElement('div');
+  newColumn.className = 'column';
+
+  const columnHeader = document.createElement('div');
+  columnHeader.className = 'column__header';
+
+  newColumn.appendChild(columnHeader);
+
+  const columnTitle = document.createElement('div');
+  columnTitle.className = 'column__title';
+  columnTitle.innerHTML = 'To do';
+
+  columnHeader.appendChild(columnTitle);
+
+  const columnButtons = document.createElement('div');
+  columnButtons.className = 'column__buttons';
+
+  columnHeader.appendChild(columnButtons);
+
+  const columnNumber = document.createElement('div');
+  columnNumber.className = 'column__number';
+  columnNumber.innerHTML = 1;
+
+  columnTitle.prepend(columnNumber);
+
+  const addButton = document.createElement('button');
+  addButton.className = 'btn show-inputs-btn';
+
+  columnButtons.appendChild(addButton);
+
+  const addIcon = document.createElement('i');
+  addIcon.className = 'fas fa-plus';
+
+  addButton.appendChild(addIcon);
+
+  function removeColumn() {
+    newColumn.parentNode.removeChild(newColumn);
+  }
+
+  const trashButton = document.createElement('button');
+  trashButton.className = 'btn';
+  trashButton.onclick = removeColumn;
+
+  columnButtons.appendChild(trashButton);
+
+  const trashIcon = document.createElement('i');
+  trashIcon.className = 'fas fa-trash-alt';
+
+  trashButton.appendChild(trashIcon);
+
+  container.insertBefore(newColumn, button);
 }
 
 button.onclick = addColumn;
