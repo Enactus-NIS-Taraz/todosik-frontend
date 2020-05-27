69ef8b008ac7de0fef07f4ddf26b5294931017ec
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2020-05-27 23:32:56 +0600

    fix: delete unnessesary parts of code

--- src/js/cardAdd.js
+++ src/js/cardAdd.js
@@ -1,114 +1,52 @@
-// const columns = document.querySelectorAll('.column');
-
-// function addCard(column) {
-//   const showInputsBtn = column.querySelector('.show-inputs-btn');
-//   const input = column.querySelector('.inputs');
-//   showInputsBtn.addEventListener('click', () => {
-//     input.classList.toggle('show-inputs');
-//   });
-// }
-
-// columns.forEach(addCard);
-
-// const column = document.getElementById('column');
-
-// const newCard = document.createElement('div');
-// newCard.className = 'inputs';
-
-// const cardHeader = document.createElement('h2');
-// cardHeader.className = 'inputs__header';
-// cardHeader.innerText = 'Create card';
-
-// newCard.appendChild(cardHeader);
-
-// const inputTitle = document.createElement('input');
-// inputTitle.className = 'inputs__item';
-// inputTitle.type = 'text';
-// inputTitle.placeholder = 'Title...';
-
-// newCard.appendChild(inputTitle);
-
-// const inputDescription = document.createElement('input');
-// inputDescription.className = 'inputs__item';
-// inputDescription.type = 'text';
-// inputDescription.placeholder = 'Description...';
-
-// newCard.appendChild(inputDescription);
-
-// const inputTime = document.createElement('input');
-// inputTime.className = 'inputs__item';
-// inputTime.type = 'number';
-// inputTime.placeholder = 'Estimated lead time';
-
-// newCard.appendChild(inputTime);
-
-// const addButton = document.createElement('button');
-// addButton.className = 'inputs__button input__button_add';
-// addButton.innerText = 'Add';
-
-// newCard.appendChild(addButton);
-
-// const cancelButton = document.createElement('button');
-// cancelButton.className = 'inputs__button';
-// cancelButton.innerText = 'Cancel';
-
-// newCard.appendChild(cancelButton);
-
-// function addNewCard() {
-//   column.appendChild(newCard);
-// }
-
-// column.addEventListener('click', addNewCard);
-
 function addNewCard(column) {
-    const newCard = document.createElement('div');
-    newCard.className = 'inputs';
-  
-    const cardHeader = document.createElement('h2');
-    cardHeader.className = 'inputs__header';
-    cardHeader.innerText = 'Create card';
-  
-    newCard.appendChild(cardHeader);
-  
-    const inputTitle = document.createElement('input');
-    inputTitle.className = 'inputs__item';
-    inputTitle.type = 'text';
-    inputTitle.placeholder = 'Title...';
-  
-    newCard.appendChild(inputTitle);
-  
-    const inputDescription = document.createElement('input');
-    inputDescription.className = 'inputs__item';
-    inputDescription.type = 'text';
-    inputDescription.placeholder = 'Description...';
-  
-    newCard.appendChild(inputDescription);
-  
-    const inputTime = document.createElement('input');
-    inputTime.className = 'inputs__item';
-    inputTime.type = 'number';
-    inputTime.placeholder = 'Estimated lead time';
-  
-    newCard.appendChild(inputTime);
-  
-    const addNewCardButton = document.createElement('button');
-    addNewCardButton.className = 'inputs__button input__button_add';
-    addNewCardButton.innerText = 'Add';
-  
-    newCard.appendChild(addNewCardButton);
-  
-    const cancelButton = document.createElement('button');
-    cancelButton.className = 'inputs__button';
-    cancelButton.innerText = 'Cancel';
-  
-    newCard.appendChild(cancelButton);
-  
-    function cancelCardAdding() {
-      newCard.parentNode.removeChild(newCard);
-    }
-    cancelButton.addEventListener('click', cancelCardAdding);
-  
-    column.appendChild(newCard);
-  }
+  const newCard = document.createElement('div');
+  newCard.className = 'inputs';
 
-export {addNewCard};
+  const cardHeader = document.createElement('h2');
+  cardHeader.className = 'inputs__header';
+  cardHeader.innerText = 'Create card';
+
+  newCard.appendChild(cardHeader);
+
+  const inputTitle = document.createElement('input');
+  inputTitle.className = 'inputs__item';
+  inputTitle.type = 'text';
+  inputTitle.placeholder = 'Title...';
+
+  newCard.appendChild(inputTitle);
+
+  const inputDescription = document.createElement('input');
+  inputDescription.className = 'inputs__item';
+  inputDescription.type = 'text';
+  inputDescription.placeholder = 'Description...';
+
+  newCard.appendChild(inputDescription);
+
+  const inputTime = document.createElement('input');
+  inputTime.className = 'inputs__item';
+  inputTime.type = 'number';
+  inputTime.placeholder = 'Estimated lead time';
+
+  newCard.appendChild(inputTime);
+
+  const addNewCardButton = document.createElement('button');
+  addNewCardButton.className = 'inputs__button input__button_add';
+  addNewCardButton.innerText = 'Add';
+
+  newCard.appendChild(addNewCardButton);
+
+  const cancelButton = document.createElement('button');
+  cancelButton.className = 'inputs__button';
+  cancelButton.innerText = 'Cancel';
+
+  newCard.appendChild(cancelButton);
+
+  function cancelCardAdding() {
+    newCard.parentNode.removeChild(newCard);
+  }
+  cancelButton.addEventListener('click', cancelCardAdding);
+
+  column.appendChild(newCard);
+}
+
+export { addNewCard };
--- src/js/removeColumn.js
+++ src/js/removeColumn.js
@@ -1,5 +1,5 @@
 function removeColumn(column) {
-    column.parentNode.removeChild(column);
-  }
+  column.parentNode.removeChild(column);
+}
 
-export {removeColumn};
+export { removeColumn };
