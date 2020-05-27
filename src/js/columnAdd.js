52447653666fc0f3ce4d614d0d73744b0240bb6d
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2020-05-27 13:44:45 +0600

    fix: fix problem with decomposition

--- src/js/columnAdd.js
+++ src/js/columnAdd.js
@@ -2,6 +2,60 @@ const button = document.getElementById('button');
 
 const container = document.getElementById('container');
 
+function removeColumn(column) {
+  column.parentNode.removeChild(column);
+}
+
+function addNewCard(column) {
+  const newCard = document.createElement('div');
+  newCard.className = 'inputs';
+
+  const cardHeader = document.createElement('h2');
+  cardHeader.className = 'inputs__header';
+  cardHeader.innerText = 'Create card';
+
+  newCard.appendChild(cardHeader);
+
+  const inputTitle = document.createElement('input');
+  inputTitle.className = 'inputs__item';
+  inputTitle.type = 'text';
+  inputTitle.placeholder = 'Title...';
+
+  newCard.appendChild(inputTitle);
+
+  const inputDescription = document.createElement('input');
+  inputDescription.className = 'inputs__item';
+  inputDescription.type = 'text';
+  inputDescription.placeholder = 'Description...';
+
+  newCard.appendChild(inputDescription);
+
+  const inputTime = document.createElement('input');
+  inputTime.className = 'inputs__item';
+  inputTime.type = 'number';
+  inputTime.placeholder = 'Estimated lead time';
+
+  newCard.appendChild(inputTime);
+
+  const addNewCardButton = document.createElement('button');
+  addNewCardButton.className = 'inputs__button input__button_add';
+  addNewCardButton.innerText = 'Add';
+
+  newCard.appendChild(addNewCardButton);
+
+  const cancelButton = document.createElement('button');
+  cancelButton.className = 'inputs__button';
+  cancelButton.innerText = 'Cancel';
+
+  newCard.appendChild(cancelButton);
+
+  function cancelCardAdding() {
+    newCard.parentNode.removeChild(newCard);
+  }
+  cancelButton.addEventListener('click', cancelCardAdding);
+
+  column.appendChild(newCard);
+}
 
 function addColumn() {
   const newColumn = document.createElement('div');
@@ -44,10 +98,9 @@ function addColumn() {
 
   columnButtons.appendChild(trashButton);
 
-  function removeColumn() {
-    newColumn.parentNode.removeChild(newColumn);
-  }
-  trashButton.onclick = removeColumn;
+  trashButton.addEventListener('click', () => {
+    removeColumn(newColumn);
+  });
 
   const trashIcon = document.createElement('i');
   trashIcon.className = 'fas fa-trash-alt';
@@ -56,58 +109,8 @@ function addColumn() {
 
   container.insertBefore(newColumn, button);
 
-
-  function addNewCard() {
-    const newCard = document.createElement('div');
-    newCard.className = 'inputs';
-
-    const cardHeader = document.createElement('h2');
-    cardHeader.className = 'inputs__header';
-    cardHeader.innerText = 'Create card';
-
-    newCard.appendChild(cardHeader);
-
-    const inputTitle = document.createElement('input');
-    inputTitle.className = 'inputs__item';
-    inputTitle.type = 'text';
-    inputTitle.placeholder = 'Title...';
-
-    newCard.appendChild(inputTitle);
-
-    const inputDescription = document.createElement('input');
-    inputDescription.className = 'inputs__item';
-    inputDescription.type = 'text';
-    inputDescription.placeholder = 'Description...';
-
-    newCard.appendChild(inputDescription);
-
-    const inputTime = document.createElement('input');
-    inputTime.className = 'inputs__item';
-    inputTime.type = 'number';
-    inputTime.placeholder = 'Estimated lead time';
-
-    newCard.appendChild(inputTime);
-
-    const addNewCardButton = document.createElement('button');
-    addNewCardButton.className = 'inputs__button input__button_add';
-    addNewCardButton.innerText = 'Add';
-
-    newCard.appendChild(addNewCardButton);
-
-    const cancelButton = document.createElement('button');
-    cancelButton.className = 'inputs__button';
-    cancelButton.innerText = 'Cancel';
-
-    newCard.appendChild(cancelButton);
-
-    function cancelCardAdding() {
-      newCard.parentNode.removeChild(newCard);
-    }
-    cancelButton.addEventListener('click', cancelCardAdding);
-
-    newColumn.appendChild(newCard);
-  }
-
-  addButton.addEventListener('click', addNewCard);
+  addButton.addEventListener('click', () => {
+    addNewCard(newColumn);
+  });
 }
 button.onclick = addColumn;
