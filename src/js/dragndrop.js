a3725f8194368e5ebd23b4ae9a4448e10aff09bf
Sultan <sutan.fb29@gmail.com>
2020-05-29 21:32:54 +0600

    feat: create drag and drop for one card

--- src/js/dragndrop.js
+++ src/js/dragndrop.js
@@ -1,51 +1,46 @@
 /* eslint-disable linebreak-style */
 function dragAndDrop () {
-  const cards = document.querySelectorAll('.card');
+  const card = document.querySelector('.card');
+  const cardContainers = document.querySelectorAll('.column__card-container');
 
-  cards.forEach((card) => {
-    const columns = document.querySelectorAll('.column__card-container');
-    
-    function dragStart () {
-      setTimeout(() => {
-        this.classList.add('hide-card');
-      }, 0)
-            
-    };
-    
-    function dragEnd () {
-      this.classList.remove('hide-card');
-    };
 
-    function dragOver (e) {
-      e.preventDefault();
-    };
-
-    function dragEnter (e) {
-      e.preventDefault();
-      this.classList.add('hovered-card');
-    };
-
-    function dragLeave () {
-      this.classList.remove('hovered-card');
-    }
-
-    function dragDrop () {
-      this.appendChild(card);
-      console.log(card);
-            
-      this.classList.remove('hovered-card');
-    }
-
-    columns.forEach((column) => {
-      column.addEventListener('dragover', dragOver);
-      column.addEventListener('dragenter', dragEnter);
-      column.addEventListener('dragleave', dragLeave);
-      column.addEventListener('drop', dragDrop);
-    });
-    
     card.addEventListener('dragstart', dragStart);
     card.addEventListener('dragend', dragEnd);
-  })
-}
 
+  function dragStart () {
+    setTimeout(() => {
+      this.classList.add('hide-card');
+    }, 0)
+  };
+
+  function dragEnd () {
+    this.classList.remove('hide-card');
+  };
+
+  function dragOver (e) {
+    e.preventDefault();
+  };
+
+  function dragEnter (e) {
+    e.preventDefault();
+    this.classList.add('hovered-card');
+  };
+
+  function dragLeave () {
+    this.classList.remove('hovered-card');
+  }
+
+  function dragDrop () {
+    this.appendChild(card);
+    console.log(card);
+    this.classList.remove('hovered-card');
+  }
+
+  cardContainers.forEach((container) => {
+    container.addEventListener('dragover', dragOver);
+    container.addEventListener('dragenter', dragEnter);
+    container.addEventListener('dragleave', dragLeave);
+    container.addEventListener('drop', dragDrop);
+});
+}
 export {dragAndDrop};
