78e30b98e6691577ae5d1f99a01efbb198c77125
Sultannis <sultan.fb29@gmail.com>
2020-06-09 15:41:43 +0600

    fix: hide dropdown when profile edit opened

--- src/js/editProfileSettings.js
+++ src/js/editProfileSettings.js
@@ -1,6 +1,8 @@
 const settingsButton = document.querySelector('.settings-btn');
 const modalBg = document.querySelector('.modal-bg_settings');
+const dropdown = document.querySelector('.dropdown');
 
 settingsButton.addEventListener('click', () => {
     modalBg.style.display = 'flex';
+    dropdown.style.display = 'none';
 })
